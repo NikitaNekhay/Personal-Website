d4e30774f1a8fedb4644c80205274154ef03deda
NikitaNekhay <ktofreesapiens@gmail.com>
2025-12-06 20:07:10 +0300

    2000

--- svelte.config.js
+++ svelte.config.js
@@ -14,8 +14,8 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		//dsadsa
-		adapter: vercel({
-			runtime: "nodejs20.x"
+		adapter: adapter({
+		runtime: "nodejs20.x"
 		}),
 		env:{
 			dir:'.',
