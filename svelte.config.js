332dfe8ecc005d4175abdc09ed10a183b245d83e
NikitaNekhay <ktofreesapiens@gmail.com>
2025-12-06 20:05:20 +0300

    20

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		//dsadsa
 		adapter: vercel({
-			runtime: "nodejs22.x"
+			runtime: "nodejs20.x"
 		}),
 		env:{
 			dir:'.',
